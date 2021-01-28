8362b21b4b91e11482ca7500435039d557e891e2
Louis Nantel <nantel.louis@gmail.com>
2021-01-28 10:50:05 -0500

    Changé de quoi

--- Git Workshop 2/Assets/Whatever.cs
+++ Git Workshop 2/Assets/Whatever.cs
@@ -7,7 +7,7 @@ public class Whatever : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Changement");
     }
 
     // Update is called once per frame
